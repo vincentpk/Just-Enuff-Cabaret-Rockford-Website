9acd8c9b520b6e18d7d1c69cc4945d0f5d4d3e85
Vincent Paik <vincentpaik@gmail.com>
2026-07-17 01:03:47 -0500

    Add dancer Lena with color-corrected photo

--- src/data/site.js
+++ src/data/site.js
@@ -54,6 +54,7 @@ export const serviceAreas = [
 // and set `image` to its path. Leave image: null to show a "coming soon" slot.
 export const dancers = [
   { name: 'April', image: '/images/dancers/april.jpg' },
+  { name: 'Lena', image: '/images/dancers/lena.jpg' },
   { name: 'Marie', image: '/images/dancers/marie.jpg' },
   { name: 'Scarlett', image: null },
   { name: 'Vivienne', image: null },
